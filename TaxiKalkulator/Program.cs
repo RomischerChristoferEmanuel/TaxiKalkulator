e9c0ba02ed09d94329cd7875b7bbace4574f3b6e
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-17 13:26:50 +0200

    befejezes

--- TaxiKalkulator/Program.cs
+++ TaxiKalkulator/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿//2. feladat
+string utasnev = "Márton Károly";
+double tavolsag = 14;
+bool ejszak = true;
+double alapdij = 1100;
+double kmdij = 440;
+double potlek = 1.20;
+
+double osszeg = kmdij * tavolsag + alapdij;
+
+//3. feladat
+if (ejszak == true)
+{
+    double vegosszeg = osszeg * potlek;
+    Console.WriteLine("Utas neve: Márton Károly");
+    Console.WriteLine($"Megtett távolság: {tavolsag}");
+    Console.WriteLine($"Alapérték: {osszeg}");
+    Console.WriteLine($"Fizetendő összeg: {vegosszeg}");
+    Console.WriteLine("Sátusz: 20% éjszakai pótlék felszámolva.");
+}
+//4. feladat
+if (ejszak == false)
+{
+    Console.WriteLine("Utas neve: Márton Károly");
+    Console.WriteLine($"Megtett távolság: {tavolsag}");
+    Console.WriteLine($"Alapérték: {osszeg}");
+    Console.WriteLine($"Fizetendő összeg: {osszeg}");
+    Console.WriteLine("Sátusz: 20% éjszakai pótlék nincs.");
+}
