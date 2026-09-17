f3005fc9585d317aebf09030a39d8b30c8ccca7d
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-17 12:35:38 +0200

    elso if és adatok

--- TaxiKalkulator/Program.cs
+++ TaxiKalkulator/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string utasnev = "Márton Károly";
+double tavolsag = 14;
+bool ejszak = true;
+double alapdij = 1100;
+double kmdij = 440;
+double potlek = 1.20;
+
+if (ejszak==true)
+{
+    double osszeg = kmdij * tavolsag + alapdij;
+
+}
